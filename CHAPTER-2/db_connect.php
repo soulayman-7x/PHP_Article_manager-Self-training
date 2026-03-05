3da3e3a10b665ceecf89b40b43f2d617b54d87bc
soulayman-7x <idbahasevenx@gmail.com>
2026-03-05 12:06:29 +0000

    Add database connection script for Chapter 2

---
+++ CHAPTER-2/db_connect.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$db = '7x_blog';
+$username = 'root';
+$password = '';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection Filed: " . $e->getMessage());
+}
